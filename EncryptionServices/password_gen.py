efc29de43d04900bafdf0517d1d4fdea032e44cb
RohanKumarSachdeva <rohan231094@gmail.com>
2021-11-15 16:23:44 -0500

    Added comments and length parameter to password_gen function

--- EncryptionServices/password_gen.py
+++ EncryptionServices/password_gen.py
@@ -1,10 +1,15 @@
 import requests
 
-def password_gen():
-    params = {"num": True, "char":True, "caps":True, "len":15}
+
+def password_gen(length=12):
+    """
+    Uses external API to generate password
+    :param length:
+    :return:
+    """
+    params = {"num": True, "char": True, "caps": True, "len": length}
     response = requests.get("https://passwordinator.herokuapp.com/generate", params=params)
     password = response.json()
     print(password)
 
     return password
-
