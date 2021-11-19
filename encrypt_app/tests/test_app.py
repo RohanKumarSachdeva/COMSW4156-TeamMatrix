30a4f9fb82b3ed61f0f8e50a27b4d806813e69c1
Rishabh Gupta <rg3334@columbia.edu>
2021-11-19 15:33:06 -0500

    shifted test_app.py inside tests folder

--- encrypt_app/tests/test_app.py
+++ encrypt_app/tests/test_app.py
@@ -1,6 +1,10 @@
 import unittest
-from app import get_app, clear_app
 import json
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+from app import get_app, clear_app
 
 
 class test_app(unittest.TestCase):
