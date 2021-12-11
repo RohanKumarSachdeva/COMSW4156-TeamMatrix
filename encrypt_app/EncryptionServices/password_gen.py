c3c07baa9d75ae0a1961e73f0659fd7d6c24ef7f
Nirvishi Shrivastava <ns3499@columbia.edu>
2021-12-11 18:43:57 -0500

    modified password_gen method to accept arguments to control if numbers, special chars and uppercase are included in password

--- encrypt_app/EncryptionServices/password_gen.py
+++ encrypt_app/EncryptionServices/password_gen.py
@@ -1,13 +1,23 @@
 import requests
 
 
-def password_gen(length=12):
+def password_gen(length=12, num=True, spchar=True, caps=True):
     """
     Uses external API to generate password
-    :param length:
+    :param length: password length
+    :param num: boolean to allow numbers in password
+    :param spchar: boolean to allow special chars in password
+    :param caps: boolean to allow uppercase letters in password
     :return:
     """
-    params = {"num": True, "char": True, "caps": True, "len": length}
+    params = {'len': length}
+    if num:
+        params['num'] = True
+    if spchar:
+        params['char'] = True
+    if caps:
+        params['caps'] = True
+
     response = requests.get("https://passwordinator.herokuapp.com/generate",
                             params=params)
     password = response.json()['data']
