9270ef0dbc6a39e11f4fec42c8359b3b02b0adba
Mausam Agrawal <mausam2906@gmail.com>
2021-12-11 02:04:10 -0500

    Change in yaml file

--- encrypt_app/tests/test_db.py
+++ encrypt_app/tests/test_db.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 import db
 
 # Commit for testing
-# Commiting to test github actions
+# Commiting to test github actions 1
 class Test_Testdb(unittest.TestCase):
 
     def setUp(self):
