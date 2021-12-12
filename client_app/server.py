065390e2ea572c49525207c2e8838c8fec3a7dfc
Rishabh Gupta <rg3334@columbia.edu>
2021-12-12 00:10:57 -0500

    added page for updating passwords

--- client_app/server.py
+++ client_app/server.py
@@ -32,7 +32,6 @@ def password_gen():
     caps_bool = request.form.get('caps', '')
     spchar_bool = request.form.get('spchar', '')
     num_bool = request.form.get('num', '')
-    print(caps_bool, spchar_bool, num_bool)
 
     query_params = dict()
     if num_bool:
@@ -48,6 +47,27 @@ def password_gen():
     generated_password = json.loads(response.text)['data']
     flash(f"Generated Password: {generated_password}")
     return render_template("generator.html")
+
+
+@app.route('/update', methods=['GET', 'POST'])
+def update_password():
+
+    if request.method == 'POST':
+        payload = dict()
+        payload['password'] = request.form['password']
+        payload['application'] = request.form['application']
+        response = requests.post(MATRIX_PASSWORD_MANAGEMENT_API + '/update',
+                                params=payload)
+        message = json.loads(response.text)['data']
+        if message:
+            flash(message)
+
+    response = requests.get(MATRIX_PASSWORD_MANAGEMENT_API + '/retrieve',
+                            params={'application': 'all'})
+    results = json.loads(response.text)['data']
+    data = [key for key in results]
+
+    return render_template("update.html", data=data)
 
 
 if __name__ == "__main__":
