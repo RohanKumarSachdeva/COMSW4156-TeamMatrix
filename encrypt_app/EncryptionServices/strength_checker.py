b1e2feaf63102395ffd049fa1dfca1b7d3ac4ed7
RohanKumarSachdeva <rohan231094@gmail.com>
2021-12-11 19:07:42 -0500

    Added return statements

---
+++ encrypt_app/EncryptionServices/strength_checker.py
@@ -0,0 +1,19 @@
+import json
+
+import requests
+
+
+def strength_checker(password):
+
+    url = "https://password-utils.p.rapidapi.com/password/check/"
+    data = {"password": password}
+    payload = json.dumps(data)
+    headers = {
+        'content-type': "application/json",
+        'x-rapidapi-host': "password-utils.p.rapidapi.com",
+        'x-rapidapi-key': "afd78c5abcmsh505035d57d05aa1p1529dajsn642939b5585e"
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+
+    return response.json()
