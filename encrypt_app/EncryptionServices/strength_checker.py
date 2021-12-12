7fd1ce04702d8463210dee8405e401b024915ccf
RohanKumarSachdeva <rohan231094@gmail.com>
2021-12-11 19:36:05 -0500

    Added comments

--- encrypt_app/EncryptionServices/strength_checker.py
+++ encrypt_app/EncryptionServices/strength_checker.py
@@ -1,10 +1,14 @@
 import json
-
 import requests
 
 
 def strength_checker(password):
-
+    """
+    Checks strength of the given password and
+    returns score, verdict and number of guesses.
+    :param password:
+    :return:
+    """
     url = "https://password-utils.p.rapidapi.com/password/check/"
     data = {"password": password}
     payload = json.dumps(data)
